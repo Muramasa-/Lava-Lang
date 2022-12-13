866b69e90ef926283ac06fdc0480e496e157e1c0
Muramasa <haydenkilloh@gmail.com>
2022-12-13 07:36:42 +0000

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,46 +1,32 @@
---External libraries used by the file
-import draw, mouse, color
+import draw, mouse, color --External libraries used by the file
 
---List of widgets (growing array)
-Widget[..] widgets;
+Widget[..] widgets; --List of widgets (growing array)
 
---Define simple rect class
-class Rect(int x, int y, int w, int h)
-    --Check if one rect is inside another's bounds
-    bool inBounds = Rect r > (x >= r.x) && (x < (r.x + r.w)) && (y >= r.y) && (y < (r.y + r.h))
+class Rect(int x, int y, int w, int h) --Define simple rect class
+    bool inBounds(Rect r) => (x >= r.x) && (x < (r.x + r.w)) && (y >= r.y) && (y < (r.y + r.h)) --Check if one rect is inside another's bounds
 end
 
---Define base widget class inheriting from rect and passing params to it via super
---Also using the trait Interact
-class Widget(int x, int y, int w, int h) := Rect() :: Interact
+class Widget(int x, int y, int w, int h) := Rect() :: Interact --Define widget inheriting from rect and passing args via super, with Interact trait
     color = color.Grey --Define new member color
     void init()
-    --Define draw method
     void draw() => drawRect(x, y, w, h, color) --Use drawRect from the imported draw lib
 end
 
---Define new interact trait with one void method
-trait Interact
+trait Interact --Define new interact trait with one void method
     void onHover()
 end
 
---Define new widget class
 class Console(str id) := Widget() --Extending Widget: := denoting it requires and passes the params
-    --Overriding the onHover method from the Interact trait
-    --Setting member with ternary operator
-    void onHover = bool state > color = state ? color.LightGrey : color.Grey
+    void onHover(bool state) => color = state ? color.LightGrey : color.Grey --Overriding the onHover method from the Interact trait
+    void draw() => @.draw(); y-- --Override the draw method, while also calling super, and decrementing the y coord
 end
 
---Init method called only once
-void onInit()
+void onInit() --Init method called only once
     widgets += Widget(0, 0, 400, 400) --Instantiate standard widget
     widgets += Console("console.1", 0, 300, 100, 400) --Instantiate custom widget
-    widgets.in(w > w.init()) --Init all widgets
+    widgets.in(w => w.init()) --Init all widgets
 end
 
---Update method called every frame
-void onUpdate()
-    --Iterate over all Widget objects in list
-    --Call instance method (defined by trait) and passing value
-    widgets.in(w > w.onHover(mouse.r.inBounds(w.r)); w.draw())
+void onUpdate() --Update method called every frame
+    widgets.in(w => w.onHover(mouse.r.inBounds(w.r)); w.draw()) --Iterate over all Widget objects in list, Call trait method and pass value
 end
